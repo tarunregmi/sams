921b6b2b2874a8b027c74de8f3c72f18b1855659
Sumiran Dahal <sumdahl@Sumirans-MacBook-Air.local>
2025-04-27 17:07:50 +0545

    changed topic what learned

--- db/database.sql
+++ db/database.sql
@@ -68,5 +68,5 @@ INSERT INTO Teach (teacher_id, course_id) value (8, 1);
 INSERT INTO Teach (teacher_id, course_id) value (9, 2);
 INSERT INTO Teach (teacher_id, course_id) value (10, 3);
 
-INSERT INTO Lesson (lesson_name, course_id, time_started, time_ended, lesson_date) value ("Linked List", 1, "7:00", "8:00", "2025-03-28");
+INSERT INTO Lesson (lesson_name, course_id, time_started, time_ended, lesson_date) value ("CPU Scheduling", 1, "7:00", "8:00", "2025-03-28");
 INSERT INTO Attendance (student_id, lesson_id, attendance_time, attendance_date, attendance_status) value (1, 1, "7:20", "2025-02-14", "Present");
